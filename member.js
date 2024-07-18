f7c574f31f81bd9683840638570e71c6f472262f
copilotfreeaccount <lajoli94@gmail.com>
2024-07-18 18:37:18 +0000

    memers

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    let member = {
+        name: 'John',
+        age: 28,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 20000
+    };
+
+    return member;
+}
